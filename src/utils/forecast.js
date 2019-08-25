b0f9524fe531683907b303309ad0e1bf66edc8b7
harshendrashah <harshendrashah812@gmail.com>
2019-08-25 20:32:36 +0530

    update in forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -12,7 +12,11 @@ const forecast = (latitude, longitude, callback) => {
             callback(undefined, body.daily.data[0].summary 
                 + ' It is currently ' 
                 + body.currently.temperature 
-                + ' degrees out. There is ' 
+                + ' degrees out. The high today is ' 
+                + body.daily.data[0].temperatureHigh 
+                + ' with a low of '
+                + body.daily.data[0].temperatureLow 
+                + '. There is ' 
                 + body.currently.precipProbability 
                 + '% chance of rain.')
         }
